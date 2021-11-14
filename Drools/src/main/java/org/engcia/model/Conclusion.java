34b61f303ab3b98c56271397f121226f420b0dae
João Silva <1150425@isep.ipp.pt>
2021-11-14 22:39:01 +0000

    Values Updated to Real Values

--- Drools/src/main/java/org/engcia/model/Conclusion.java
+++ Drools/src/main/java/org/engcia/model/Conclusion.java
@@ -1,67 +1,53 @@
 package org.engcia.model;
 
 public class Conclusion {
-    public static final String AOCSThermalSensorError = "AOCS Thermal Partial Failure";
+    public static final String AOCSThermalSensorError = "AOCS Thermal Sensor Error: ";
     public static final String AOCSThermalSubSystemFailure = "AOCS Thermal Full Failure";
-    public static final String AOCSPowerSensorError = "AOCS Power Partial Failure";
-    public static final String AOCSPowerSubSystemFailure = "AOCS Power Full Failure";
-    public static final String AOCSVoltageSensorError = "AOCS Voltage Partial Failure";
+    public static final String AOCSVoltageSensorError = "AOCS Voltage Sensor Error: ";
     public static final String AOCSVoltageSubSystemFailure = "AOCS Voltage Full Failure";
-    public static final String AOCSCurrentSensorError = "AOCS Current Partial Failure";
+    public static final String AOCSCurrentSensorError = "AOCS Current Sensor Error: ";
     public static final String AOCSCurrentSubSystemFailure = "AOCS Current Full Failure";
 
-    public static final String PayloadThermalSensorError = "Payload Thermal Partial Failure";
+    public static final String PayloadThermalSensorError = "Payload Thermal Sensor Error: ";
     public static final String PayloadThermalSubSystemFailure = "Payload Thermal Full Failure";
-    public static final String PayloadPowerSensorError = "Payload Power Partial Failure";
-    public static final String PayloadPowerSubSystemFailure = "Payload Power Full Failure";
-    public static final String PayloadVoltageSensorError = "Payload Voltage Partial Failure";
+    public static final String PayloadVoltageSensorError = "Payload Voltage Sensor Error: ";
     public static final String PayloadVoltageSubSystemFailure = "Payload Voltage Full Failure";
-    public static final String PayloadCurrentSensorError = "Payload Current Partial Failure";
+    public static final String PayloadCurrentSensorError = "Payload Current Sensor Error: ";
     public static final String PayloadCurrentSubSystemFailure = "Payload Current Full Failure";
 
-    public static final String SwitchThermalSensorError = "Switch Module Thermal Partial Failure";
+    public static final String SwitchThermalSensorError = "Switch Module Thermal Sensor Error: ";
     public static final String SwitchThermalSubSystemFailure = "Switch Module Thermal Full Failure";
-    public static final String SwitchPowerSensorError = "Switch Module Power Partial Failure";
-    public static final String SwitchPowerSubSystemFailure = "Switch Module Power Full Failure";
-    public static final String SwitchVoltageSensorError = "Switch Module Voltage Partial Failure";
+    public static final String SwitchVoltageSensorError = "Switch Module Voltage Sensor Error: ";
     public static final String SwitchVoltageSubSystemFailure = "Switch Module Voltage Full Failure";
-    public static final String SwitchCurrentSensorError = "Switch Module Current Partial Failure";
+    public static final String SwitchCurrentSensorError = "Switch Module Current Sensor Error: ";
     public static final String SwitchCurrentSubSystemFailure = "Switch Module Current Full Failure";
 
-    public static final String OBCThermalSensorError = "OBC Thermal Partial Failure";
+    public static final String OBCThermalSensorError = "OBC Thermal Sensor Error: ";
     public static final String OBCThermalSubSystemFailure = "OBC Thermal Full Failure";
-    public static final String OBCPowerSensorError = "OBC Power Partial Failure";
-    public static final String OBCPowerSubSystemFailure = "OBC Power Full Failure";
-    public static final String OBCVoltageSensorError = "OBC Voltage Partial Failure";
+    public static final String OBCVoltageSensorError = "OBC Voltage Sensor Error: ";
     public static final String OBCVoltageSubSystemFailure = "OBC Voltage Full Failure";
-    public static final String OBCCurrentSensorError = "OBC Current Partial Failure";
+    public static final String OBCCurrentSensorError = "OBC Current Sensor Error: ";
     public static final String OBCCurrentSubSystemFailure = "OBC Current Full Failure";
 
-    public static final String CommunicationThermalSensorError = "Communication Thermal Partial Failure";
+    public static final String CommunicationThermalSensorError = "Communication Thermal Sensor Error: ";
     public static final String CommunicationThermalSubSystemFailure = "Communication Thermal Full Failure";
-    public static final String CommunicationPowerSensorError = "Communication Power Partial Failure";
-    public static final String CommunicationPowerSubSystemFailure = "Communication Power Full Failure";
-    public static final String CommunicationVoltageSensorError = "Communication Voltage Partial Failure";
+    public static final String CommunicationVoltageSensorError = "Communication Voltage Sensor Error: ";
     public static final String CommunicationVoltageSubSystemFailure = "Communication Voltage Full Failure";
-    public static final String CommunicationCurrentSensorError = "Communication Current Partial Failure";
+    public static final String CommunicationCurrentSensorError = "Communication Current Sensor Error: ";
     public static final String CommunicationCurrentSubSystemFailure = "Communication Current Full Failure";
 
-    public static final String ThermalControllerThermalSensorError = "Thermal Controller Thermal Partial Failure";
+    public static final String ThermalControllerThermalSensorError = "Thermal Controller Thermal Sensor Error: ";
     public static final String ThermalControllerThermalSubSystemFailure = "Thermal Controller Thermal Full Failure";
-    public static final String ThermalControllerPowerSensorError = "Thermal Controller Power Partial Failure";
-    public static final String ThermalControllerPowerSubSystemFailure = "Thermal Controller Power Full Failure";
-    public static final String ThermalControllerVoltageSensorError = "Thermal Controller Voltage Partial Failure";
+    public static final String ThermalControllerVoltageSensorError = "Thermal Controller Voltage Sensor Error: ";
     public static final String ThermalControllerVoltageSubSystemFailure = "Thermal Controller Voltage Full Failure";
-    public static final String ThermalControllerCurrentSensorError = "Thermal Controller Current Partial Failure";
+    public static final String ThermalControllerCurrentSensorError = "Thermal Controller Current Sensor Error: ";
     public static final String ThermalControllerCurrentSubSystemFailure = "Thermal Controller Current Full Failure";
 
-    public static final String PropulsionThermalSensorError = "Propulsion Thermal Partial Failure";
+    public static final String PropulsionThermalSensorError = "Propulsion Thermal Sensor Error: ";
     public static final String PropulsionThermalSubSystemFailure = "Propulsion Thermal Full Failure";
-    public static final String PropulsionPowerSensorError = "Propulsion Power Partial Failure";
-    public static final String PropulsionPowerSubSystemFailure = "Propulsion Power Full Failure";
-    public static final String PropulsionVoltageSensorError = "Propulsion Voltage Partial Failure";
+    public static final String PropulsionVoltageSensorError = "Propulsion Voltage Sensor Error: ";
     public static final String PropulsionVoltageSubSystemFailure = "Propulsion Voltage Full Failure";
-    public static final String PropulsionCurrentSensorError = "Propulsion Current Partial Failure";
+    public static final String PropulsionCurrentSensorError = "Propulsion Current Sensor Error: ";
     public static final String PropulsionCurrentSubSystemFailure = "Propulsion Current Full Failure";
 
     private String description;
