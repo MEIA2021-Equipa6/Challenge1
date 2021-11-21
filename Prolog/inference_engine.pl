f95460a092ee1df4bddf59068dd87c90d38b4514
jMoreiraSantos <51131101+jMoreiraSantos@users.noreply.github.com>
2021-11-21 21:37:41 +0000

    Add Final modifications before presentation of the PROLOG E.S

--- Prolog/inference_engine.pl
+++ Prolog/inference_engine.pl
@@ -29,7 +29,19 @@ start_engine:-
 		aocs_gps(_, _), aocs_gpsFailure(_, _), aocs_star_tracker(_, _), power_current_output(_, _), 
 		power_currentOutputFailure(_, _), power_battery_status(_, _), power_batteryFailure(_, _), 
 		power_batteryWarning(_, _), power_current_input(_, _), power_currentInputFailure(_, _), 
-		obc_battery_status(_, _)
+		obc_battery_status(_, _), aocs_navigationFailure(_,_), aocs_reaction_wheel(_,_), aocs_thruster(_,_),
+		power_thermal(_,_), power_thermalFailure(_,_), power_tension(_,_), power_tensionFailure(_,_),
+		power_current(_,_), power_currentFailure(_,_), power_switch(_,_), power_switchFailure(_,_),
+		power_switchFailure(_,_), power_switchFailure(_,_), power_switchFailure(_,_),
+		payload_thermal(_,_), payload_thermalFailure(_,_), payload_tension(_,_), payload_tensionFailure(_,_),
+		payload_current(_,_), payload_currentFailure(_,_), payload_switch(_,_), payload_switchFailure(_,_),
+		payload_switchFailure(_,_), payload_switchFailure(_,_), payload_switchFailure(_,_),
+		communication_thermal(_,_), communication_thermalFailure(_,_), communication_tension(_,_), communication_tensionFailure(_,_),
+		communication_current(_,_), communication_currentFailure(_,_), communication_switch(_,_), communication_switchFailure(_,_),
+		communication_groundConnectionFailure(_,_),
+		obc_thermal(_,_), obc_thermalFailure(_,_), obc_tension(_,_), obc_tensionFailure(_,_),
+		obc_current(_,_), obc_currentFailure(_,_), obc_switch(_,_), obc_switchFailure(_,_), obc_ground_connection(_,_), 
+		payload(_,_), communication(_,_), power(_,_), obc(_,_)
 	]),
 	fact(N,Fact),
 	fact_triggers_rules1(Fact, LRules),
@@ -63,15 +75,21 @@ trigger_rules(_, _, []).
 
 fact_is_in_condition(F,[F  and _]).
 fact_is_in_condition(F,[evaluate(F1) and _]):- F=..[H,H1|_],F1=..[H,H1|_].
+fact_is_in_condition(F,[evaluate_fact_values(F1,_,_) and _]):- F=..[H,H1|_],F1=..[H,H1|_].
+fact_is_in_condition(F,[evaluate_fact_values(_,_,F1) and _]):- F=..[H,H1|_],F1=..[H,H1|_].
 fact_is_in_condition(F,[_ and Fs]):- fact_is_in_condition(F,[Fs]).
 
 fact_is_in_condition(F,[F  or _]).
 fact_is_in_condition(F,[evaluate(F1) or _]):- F=..[H,H1|_], F1=..[H,H1|_], !.
+fact_is_in_condition(F,[evaluate_fact_values(F1,_,_) or _]):- F=..[H,H1|_], F1=..[H,H1|_], !.
+fact_is_in_condition(F,[evaluate_fact_values(_,_,F1) or _]):- F=..[H,H1|_], F1=..[H,H1|_], !.
 fact_is_in_condition(F,[_ or Fs]):- fact_is_in_condition(F,[Fs]).
 
 fact_is_in_condition(F,[F]):-!.
 
 fact_is_in_condition(F,[evaluate(F1)]):-F=..[H,H1|_],F1=..[H,H1|_].
+fact_is_in_condition(F,[evaluate_fact_values(F1,_,_)]):-F=..[H,H1|_],F1=..[H,H1|_].
+fact_is_in_condition(F,[evaluate_fact_values(_,_,F1)]):-F=..[H,H1|_],F1=..[H,H1|_].
 
 % and - part1
 verify_conditions([not evaluate(X) and Y],[not X|LF]):- !,
@@ -79,6 +97,12 @@ verify_conditions([not evaluate(X) and Y],[not X|LF]):- !,
 	verify_conditions([Y],LF).
 verify_conditions([evaluate(X) and Y],[N|LF]):- !,
 	evaluate(N,X),
+	verify_conditions([Y],LF).
+
+% Added custom method that validates values from two facts
+verify_conditions([evaluate_fact_values(F1,Op,F2) and Y],LAux):- !,
+	evaluate_fact_values(F1,Op,F2, N),
+	append(N,LF, LAux),
 	verify_conditions([Y],LF).
 
 % or - part1
@@ -91,6 +115,14 @@ verify_conditions([evaluate(X) or Y],[N|LF]):-
 
 verify_conditions([not evaluate(X)],[not X]):- !, \+ evaluate(_,X).
 verify_conditions([evaluate(X)],[N]):- !, evaluate(N,X).
+
+% Added custom method that validates values from two facts
+verify_conditions([evaluate_fact_values(F1,Op,F2) or Y],LF):-
+	evaluate_fact_values(F1,Op,F2, N),
+	append(N,LF, LF);
+	verify_conditions([Y],LF).
+
+verify_conditions([evaluate_fact_values(F1,Op,F2)],N):- !, evaluate_fact_values(F1,Op,F2,N).
 
 % and - part2
 verify_conditions([not X and Y],[not X|LF]):- !,
@@ -145,6 +177,14 @@ evaluate(N,P):-	P=..[Functor,Entity,Operand,Value],
 	P1=..[Functor,Entity,Value1],
 	fact(N,P1),
 	compare2(Value1,Operand,Value).
+
+
+evaluate_fact_values(F1,Operand,F2, [N1,N2]):-
+	F1=..[_,_,Value],
+	fact(N1,F1),
+	F2=..[_,_,Value2],
+	fact(N2,F2),
+	compare2(Value,Operand,Value2),!.
 
 compare2(V1,==,V):- V1==V.
 compare2(V1,\==,V):- V1\==V.
